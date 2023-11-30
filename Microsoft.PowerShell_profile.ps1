ff5ab841545ce7749379d2d6ac646fd609f5fc1a
mogensNielsen <17141802+mogensNielsen@users.noreply.github.com>
2023-11-30 09:42:51 +0100

    Adds alias to profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,12 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\mogens.omp.json" | Invoke-E
 # replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
+# Add functions for aliases
+# Creates a function that can be called in Set-Alias
+function git-switch-main { git switch main }
+
+# Add aliases
+Set-Alias -Name main -Value git-switch-main
+
 # Change this path to where Powershell scripts are stored
 $env:path += ";H:\My Documents\work\powershell"
