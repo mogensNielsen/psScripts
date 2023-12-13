f3023dfc2a4c20220e36728695a98f2e430e2251
mogensNielsen <17141802+mogensNielsen@users.noreply.github.com>
2023-12-13 14:26:58 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,11 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\mogens.omp.json" | Invoke-E
 # replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
+# Make PSReadLine show a list of suggestions instead of just one suggestions
+Set-PSReadLineOption -PredictionViewStyle ListView
+
 # Add functions for aliases
-# Creates a function that can be called in Set-Alias
+## Creates a function that can be called in Set-Alias
 function git-switch-main { git switch main }
 
 # Add aliases
