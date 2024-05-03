87d9403ef016deba7ec5cbc5fea091fb6524c8fc
mogensNielsen <17141802+mogensNielsen@users.noreply.github.com>
2024-05-03 15:31:09 +0200

    Update fdbt.ps1
    Adds parameter to specify which dbt command to perform

--- fdbt.ps1
+++ fdbt.ps1
@@ -1,17 +1,28 @@
 # Fuzzy dbt run
-# Lists models under \models\ and pipes them to fzf
-# so that the user can fuzzy search for a model
-# Then runs that model with `dbt run -s [model name]`
-# If no model is selected (ctrl-c), it fails gracefully
+# Lists models under \models\ and pipes them to fzf so that the user can fuzzy search for a model
+# Then performs the specified dbt command on the selected model(s)
+# If no dbt command or model is selected (ctrl-c), it fails gracefully
 function fdbt {
+    param(
+        [ValidateSet('list', 'compile', 'run', 'build', 'test')]
+        [string]$command = $args[0]
+    )
+
+    if(-not $command) {
+        Write-Output "Please specify a dbt command: list, compile, run, build or test"
+        return
+    }
+
     $models = (Get-ChildItem models\ -Recurse -Include *.sql).BaseName
     $models = $models | Out-String | fzf -d $(( 2 + ($models.Count) )) -m
 
-    if($null -eq $models) {
+    if(-not $models) {
+        Write-Output "No model selected"
         return
     }
     else {
-        dbt run -s $models
+        Write-Output "Doing dbt $command for $models"
+        & dbt $command -s $models
     }
 }
 fdbt
