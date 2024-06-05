3d60c3474be6c0da6877a5f495b5cff6f9485a22
mogensNielsen <17141802+mogensNielsen@users.noreply.github.com>
2024-06-05 09:03:00 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,10 +8,15 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Add functions for aliases
 ## Creates a function that can be called in Set-Alias
+## Function to switch to main branch
 function git-switch-main { git switch main }
+
+## Function to do `git fetch && git status`
+function git-fetch-status { git fetch && git status }
 
 # Add aliases
 Set-Alias -Name main -Value git-switch-main
+Set-Alias -Name gfs -Value git-fetch-status
 
 # Change this path to where Powershell scripts are stored
 $env:path += ";[path to where you have stored your scripts]"
